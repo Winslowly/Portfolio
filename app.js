817227f395d84edc8deffb889dd29ed6612710be
Eli Winslow <eliwinslow@Elis-MacBook-Air.local>
2022-11-03 06:28:05 -0700

    Wednsday push

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const div= [
-    "homeTile",
-    "bioTile",
-    "resumeTile",
-    "projectsTile",
-    "linksTile"
-]
+// const div= [
+//     "homeTile",
+//     "bioTile",
+//     "resumeTile",
+//     "projectsTile",
+//     "linksTile"
+
+
+
+const body= (() => {     
+    $('homeTile').hide()
+    $('#bioTile').hide()
+    $('#resumeTile').hide()
+    $('#projectsTile').hide()
+    $('#linksTile').hide()  
+
+
+
+    $('#homeButton').on('click', {css.'homeTile'.display: block});
+    $('#bioButton').on('click', {display: block});
+    $('#resumeButton').on('click', {display: block});
+    $('#projectsButton').on('click', {display: block});
+    $('#linksButton').on('click', {display: block});
+})
