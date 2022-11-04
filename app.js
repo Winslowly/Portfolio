3aef1c6cb7c7220e64a4bdc27a795adeb166f10e
Eli Winslow <eliwinslow@Elis-MacBook-Air.local>
2022-11-04 06:26:11 -0700

    thursday update

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+
+$(() => {
+    $('.next').on('click', () => {
+        let currentImgIndex = 0;
+        .css('display', 'none')
+        currentImgIndex++
+        $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+        if(currentImgIndex < numOfImages) {
+            currentImgIndex ++
+           } else {
+            currentImgIndex = 0
+           }
+       }
+    
+// })
+// $((numOfImages) => {
+
+//     $('.carousel-images').children().length - 1
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(currentImgIndex < numOfImages) {
+//     currentImgIndex ++
+//    } else {
+//     currentImgIndex = 0
+//    }
+
+
+
+
+
+
+
+
+//    $('.previous').on('click', () => {
+//     // stuff will go here 
+//    }
+
 // const div= [
 //     "homeTile",
 //     "bioTile",
@@ -6,19 +59,36 @@
 //     "linksTile"
 
 
+// 
+//     display: block;
+// })
 
-const body= (() => {     
-    $('homeTile').hide()
-    $('#bioTile').hide()
-    $('#resumeTile').hide()
-    $('#projectsTile').hide()
-    $('#linksTile').hide()  
+   
+    
+   
+    // $('#resumeTile').hide()
+    // $('#projectsTile').hide()
+    // $('#linksTile').hide()   
+// $(() => {  
+//     $('#hometile').hide()
+//     $('#biotile').hide()
+//     $('#hometile').hide()
+//     $('#resumetile').hide()
+//     $('#projectstile').hide()
+
+// }
+// const showText = () => {
+        
+//     $('#biotile').slideDown(1000);
+
+//     }
+
+//     $('#bioButton').on('click',showText);
+
+    // $('#bioButton').slideDown(1000);
+    // $('#resumeButton').slideDown(1000);
+    // $('#projectsButton').slideDown(1000);
+    // $('#linksButton').slideDown(1000)
+    
 
 
-
-    $('#homeButton').on('click', {css.'homeTile'.display: block});
-    $('#bioButton').on('click', {display: block});
-    $('#resumeButton').on('click', {display: block});
-    $('#projectsButton').on('click', {display: block});
-    $('#linksButton').on('click', {display: block});
-})
